088b20fbc365ed8cb88c0c5df721f6f33d55bb1e
hws522 <sraphic@kakao.com>
2021-05-29 16:59:16 +0900

    JSX

---
+++ vlpt-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <>
+      <Hello />
+      <div>Hi</div>
+    </>
+  );
+}
+
+export default App;
