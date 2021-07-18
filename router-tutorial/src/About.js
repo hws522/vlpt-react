afeb2f708310e541be646a0cd02d4f1269aff027
hws522 <sraphic@kakao.com>
2021-07-18 14:41:29 +0900

    add <Link>, <Route>, <BrowserRouter>

---
+++ router-tutorial/src/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>ABOUT</h1>
+      <div>Test About</div>
+    </div>
+  );
+}
--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ui>
+        <li>
+          <Link to="/">HOME</Link>
+        </li>
+        <li>
+          <Link to="/about">ABOUT</Link>
+        </li>
+      </ui>
+      <hr />
+
+      <Route exact path="/" component={Home}></Route>
+      <Route path="/about" component={About}></Route>
     </div>
   );
 }
