ab206d26eb1ff69056091cc01b3e8c0847dc8d5d
hws522 <sraphic@kakao.com>
2021-08-05 17:04:50 +0900

    add redux-devtools-extension

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -4,8 +4,9 @@ import App from './App';
 import { createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
     <Provider store={store}>
