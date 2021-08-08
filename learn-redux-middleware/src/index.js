27327fcd53dcfed94d3e3981a75b5fcfc7ffaf22
hws522 <sraphic@kakao.com>
2021-08-08 17:35:05 +0900

    add middleware-ReduxThunk

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -7,8 +7,9 @@ import rootReducer from './modules';
 // import myLogger from './middlewares/myLogger';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk, logger)));
 
 ReactDOM.render(
     <Provider store={store}>
