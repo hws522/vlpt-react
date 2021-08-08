dc8d5648203264221e87b0a15b4a0bcd8e26eda5
hws522 <sraphic@kakao.com>
2021-08-08 18:21:14 +0900

    redux-thunk promise

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,10 +1,14 @@
-import CounterContainer from './containers/CounterContainer';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
 
 function App() {
     return (
-        <div>
-            <CounterContainer />
-        </div>
+        <>
+            <Route path="/" component={PostListPage} exact={true} />
+            <Route path="/:id" component={PostPage} />
+        </>
     );
 }
 
---
+++ learn-redux-middleware/src/lib/asyncUtils.js
@@ -0,0 +1,65 @@
+export const createPromiseThunk = (type, promiseCreator) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+    return (param) => async (dispatch) => {
+        dispatch({ type, param });
+        try {
+            const payload = await promiseCreator(param);
+            dispatch({ type: SUCCESS, payload }); // 성공
+        } catch (e) {
+            dispatch({ type: ERROR, payload: e, error: true }); // 실패
+        }
+    };
+};
+
+export const reducerUtils = {
+    initial: (initialData = null) => ({
+        loading: false,
+        data: initialData,
+        error: null,
+    }),
+    loading: (prevState = null) => ({
+        loading: true,
+        data: prevState,
+        error: null,
+    }),
+    // 성공 상태
+    success: (payload) => ({
+        loading: false,
+        data: payload,
+        error: null,
+    }),
+    // 실패 상태
+    error: (error) => ({
+        loading: false,
+        data: null,
+        error: error,
+    }),
+};
+
+// 비동기 관련 액션들을 처리하는 리듀서를 만들어줍니다.
+// type 은 액션의 타입, key 는 상태의 key (예: posts, post) 입니다.
+export const handleAsyncActions = (type, key) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+    return (state, action) => {
+        switch (action.type) {
+            case type:
+                return {
+                    ...state,
+                    [key]: reducerUtils.loading(),
+                };
+            case SUCCESS:
+                return {
+                    ...state,
+                    [key]: reducerUtils.success(action.payload),
+                };
+            case ERROR:
+                return {
+                    ...state,
+                    [key]: reducerUtils.error(action.payload),
+                };
+            default:
+                return state;
+        }
+    };
+};
