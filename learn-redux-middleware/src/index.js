7cf2f99eabbb9051a7d73f4e8cea5dab856ea47b
hws522 <sraphic@kakao.com>
2021-08-08 17:25:29 +0900

    add middleware-logger, add composeWithDevTools

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -4,9 +4,11 @@ import App from './App';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
-import myLogger from './middlewares/myLogger';
+// import myLogger from './middlewares/myLogger';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 ReactDOM.render(
     <Provider store={store}>
